3edb54295dae0548febe4b34d8c4c3f195595131
Sanzhik11 <sanzhikkzo111@icloud.com>
2024-10-30 21:21:50 +0600

    Add files via upload

---
+++ hw3-js/js/script.js
@@ -0,0 +1,16 @@
+var name = "аты: Али" 
+var age = "Жасы: 25"
+var isStudent = true; //bolean true/false
+
+if(isStudent){
+
+}
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+
+
+
+
